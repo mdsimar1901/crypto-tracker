2ffccd4ff0683f350e7c98c3d3b42b48f0cfb776
mdsimar1901 <66200713+mdsimar1901@users.noreply.github.com>
2022-01-15 01:03:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import "./styles.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Coin from "./Coin";
+import "./App.css";
+
+const link =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+export default function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    axios
+      .get(link)
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <form>
+          <input
+            type="text"
+            placeholder="Enter your coin here"
+            className="coin-input"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
+    </div>
+  );
+}
